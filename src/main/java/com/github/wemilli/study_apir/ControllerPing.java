d0d56c155cbe998276334694ce0a8c5b2dad742d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 21:35:42 -0300

    Primeiro commit

---
+++ src/main/java/com/github/wemilli/study_apir/ControllerPing.java
@@ -0,0 +1,19 @@
+package com.github.wemilli.study_apir;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //todas as classes de controle precisam dessa notação
+@RequestMapping("ping")
+public class ControllerPing {
+
+    @GetMapping
+    public String ping(){
+        return "pong";
+    }
+
+    public String mensagem(){
+        return "pong";
+    }
+}
